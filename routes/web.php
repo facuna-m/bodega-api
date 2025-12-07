af02fea8c6b24d0d57490ccebca7c3abe5a3b013
Felipe Acuña Morales <facuna@ing.ucsc.cl>
2025-12-07 19:42:28 -0300

    cambié las rutas para implementar vista

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,8 @@
 <?php
 
+use App\Http\Controllers\MovimientoController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [MovimientoController::class, 'index']);
+
+Route::post('/movimiento', [MovimientoController::class, 'store'])->name('movimiento.guardar');
