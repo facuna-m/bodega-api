fbf6687dad516c70fb00bc8a7126142fa853c006
Felipe Acuña Morales <facuna@ing.ucsc.cl>
2025-12-07 19:43:06 -0300

    agregué función index para mostrar los productos

--- app/Http/Controllers/MovimientoController.php
+++ app/Http/Controllers/MovimientoController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\Movimiento;
+use App\Models\Producto;
 use Illuminate\Http\Request;
 use Illuminate\Database\QueryException;
 
@@ -25,14 +26,19 @@ class MovimientoController extends Controller
             $movimiento->id_producto = $request->id_producto;
             $movimiento->save();
 
-            return response()->json(['message' => 'Movimiento creado exitosamente', 'movimiento' => $movimiento], 201);
+            return back()->with('success', 'Movimiento registrado exitosamente.');
         }
 
         // Manejar errores de base de datos
         catch(QueryException $e){
             $errorCode = $e->errorInfo[0];
             $errorMessage = $e->errorInfo[2];
-            return response()->json(['error' => 'Error de base de datos', 'detalle' => $errorMessage], 400);
+            return back()->withErrors(['database_error' => "Error Code: $errorCode, Message: $errorMessage"]);
         }
+    }
+
+    public function index(){
+        $productos = Producto::all();
+        return view('bodega', compact('productos'));
     }
 }
